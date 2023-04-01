9404d4c7db4efeecc390b3178e0d8134a40c5e7f
devdjangoreact <devdjangoreact@gmail.com>
2023-04-01 08:36:08 +0000

    test

---
+++ src/components/ui/SignOutButton.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface SignOutButtonProps {}
+
+const SignOutButton: FC<SignOutButtonProps> = ({}) => {
+  return <div>SignOutButton</div>;
+};
+
+export default SignOutButton;
